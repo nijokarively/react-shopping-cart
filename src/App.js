a3395217fa00d8787c9a5e6f2d48dcd753cbaeaa
Nijo Varghese Karively <nijovarghese.karively@bt.com>
2021-04-28 23:52:46 -0500

    implemented Routes and CartContext

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import { hot } from 'react-hot-loader/root';
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import { hot } from "react-hot-loader/root";
 
-import 'globals.styl'
-import styles from './App.styl';
+import "globals.styl";
+import styles from "./App.styl";
 
-import { apiGET } from './utils/fetch';
+import { apiGET } from "./utils/fetch";
+
+import Routes from "./Routes";
+import CartContextProvider from './contexts/CartContext';
+
 
 class App extends PureComponent {
+//   async componentDidMount() {
+//     console.log(
+//       "apiGET example:",
+//       await apiGET("products", {
+//         limit: 5,
+//         filters: {
+//           name: "apple",
+//           "nutrition.calories": "> 100",
+//         },
+//       })
+//     );
+//   }
 
-    async componentDidMount () {
-        console.log(
-            'apiGET example:',
-            await apiGET('products', {
-                limit: 5,
-                filters: {
-                    name: 'apple',
-                    'nutrition.calories': '> 100',
-                },
-            })
-        );
-    }
-
-    render () {
-        return (
-            <div className={styles.root}>
-                App Root
-            </div>
-        );
-    }
-
+  render() {
+    return (
+      <div className={styles.root}>
+        <CartContextProvider>
+          <Routes />
+        </CartContextProvider>
+      </div>
+    );
+  }
 }
 
 export default hot(App);
