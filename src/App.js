0a5aee2ec2c460605d5c74f63ef304c6ab22f215
Nijo Varghese Karively <nijovarghese.karively@bt.com>
2021-04-29 19:33:41 -0500

    formatted code

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import React, { PureComponent } from "react";
-import PropTypes from "prop-types";
 import { hot } from "react-hot-loader/root";
 
 import "globals.styl";
 import styles from "./App.styl";
 
-import { apiGET } from "./utils/fetch";
-
 import Routes from "./Routes";
-import CartContextProvider from './contexts/CartContext';
-
+import CartContextProvider from "./contexts/CartContext";
 
 class App extends PureComponent {
-//   async componentDidMount() {
-//     console.log(
-//       "apiGET example:",
-//       await apiGET("products", {
-//         limit: 5,
-//         filters: {
-//           name: "apple",
-//           "nutrition.calories": "> 100",
-//         },
-//       })
-//     );
-//   }
-
   render() {
     return (
       <div className={styles.root}>
