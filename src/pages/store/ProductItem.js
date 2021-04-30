521c11d2bedfcce15040d119f36a35c651ce31b4
Nijo Varghese Karively <nijovarghese.karively@bt.com>
2021-04-29 20:25:49 -0500

    added extra product info to card

--- src/pages/store/ProductItem.js
+++ src/pages/store/ProductItem.js
@@ -1,34 +1,68 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
+import { red } from "@material-ui/core/colors";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Button from "@material-ui/core/Button";
 
 import { CartContext } from "../../contexts/CartContext";
-import { formatNumber } from "../../utils/formatNumber";
 
-const useStyles = makeStyles({
+
+const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 345,
-    height: 345,
+    minHeight: 420,
   },
   media: {
-    height: 140,
+    height: 0,
+    paddingTop: "56.25%", // 16:9
   },
-});
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
 
 export default function ProductItem({ product }) {
   const classes = useStyles();
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   const { addProduct, cartItems, increaseItem } = useContext(CartContext);
 
   const isInCart = (product) => {
     return !!cartItems.find((item) => item.id === product.id);
   };
+
+  const allergens = product.allergens
+    ? Array.from(product.allergens).join()
+    : "N/A";
+  const badges = product.badges ? Array.from(product.badges).join() : "N/A";
+  const nutrition = product.nutrition
+    ? JSON.stringify(product.nutrition)
+    : "N/A";
+  const diets = product.diets ? Array.from(product.diets).join() : "N/A";
 
   return (
     <Card className={classes.root}>
@@ -42,8 +76,11 @@ export default function ProductItem({ product }) {
           <Typography gutterBottom variant="h5" component="h2">
             {product.name}
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {product.brand}
+          <Typography variant="h6" component="h6">
+            {product.brand || "No brand"}
+          </Typography>
+          <Typography variant="h6" component="h6">
+            {product.category || "UNCATEGORIZED"}
           </Typography>
         </CardContent>
       </CardActionArea>
@@ -51,7 +88,7 @@ export default function ProductItem({ product }) {
         {isInCart(product) && (
           <Button
             size="small"
-            variant="outlined" 
+            variant="outlined"
             color="primary"
             onClick={() => increaseItem(product)}
           >
@@ -62,14 +99,36 @@ export default function ProductItem({ product }) {
         {!isInCart(product) && (
           <Button
             size="small"
-            variant="outlined" 
+            variant="outlined"
             color="primary"
             onClick={() => addProduct(product)}
           >
             Add to cart
           </Button>
         )}
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
       </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Diets:</Typography>
+          <Typography paragraph>{diets}</Typography>
+          <Typography paragraph>Allergens:</Typography>
+          <Typography paragraph>{allergens}</Typography>
+          <Typography paragraph>Nutrition:</Typography>
+          <Typography paragraph>{nutrition}</Typography>
+          <Typography paragraph>Badges:</Typography>
+          <Typography paragraph>{badges}</Typography>
+        </CardContent>
+      </Collapse>
     </Card>
   );
 }
